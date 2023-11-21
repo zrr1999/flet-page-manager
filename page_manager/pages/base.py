ed7fa5703499a08d190be752a0f48d3fc6d56af0
Zhan Rongrui <2742392377@qq.com>
2023-11-22 00:17:40 +0800

    improve PageBase

--- page_manager/pages/base.py
+++ page_manager/pages/base.py
@@ -1,16 +1,20 @@
 from __future__ import annotations
 import flet as ft
-from ..manager import PageManager
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from ..manager import PageManager
+    from ..state import StateBase
 
 
-class PageBase:
-    async def init(self, page: ft.Page, pm: PageManager):
+class PageBase[StateT: StateBase]:
+    async def init(self, page: ft.Page, pm: PageManager[StateT]):
         pm.state.running_pages.append(page)
         await page.window_center_async()
 
-    async def build(self, page: ft.Page, pm: PageManager):
+    async def build(self, page: ft.Page, pm: PageManager[StateT]):
         raise NotImplementedError
 
-    async def __call__(self, page: ft.Page, pm: PageManager):
+    async def __call__(self, page: ft.Page, pm: PageManager[StateT]):
         await self.init(page, pm)
         await self.build(page, pm)
